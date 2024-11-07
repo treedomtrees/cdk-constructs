1f2a8ad00e81093ad56135360e32c40fd4daf9bb
Brando Meniconi <b.meniconi@treedom.net>
2024-11-07 15:39:51 +0100

    feat: add datadog constucts

---
+++ packages/datadog/src/lambdaAspect.ts
@@ -0,0 +1,47 @@
+import { IAspect } from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { IConstruct } from "constructs";
+import { DatadogLambda } from "datadog-cdk-constructs-v2";
+
+export type DatadogLambdaAspectProps = {
+  datadog: DatadogLambda;
+};
+
+export class AddDatadogToLambdas implements IAspect {
+  public readonly datadog: DatadogLambda;
+  public readonly name: string;
+
+  constructor(name: string, props: DatadogLambdaAspectProps) {
+    this.name = name;
+    this.datadog = props.datadog;
+  }
+
+  public visit(node: IConstruct): void {
+    // See that we're dealing with a CfnBucket
+    if (node instanceof lambda.Function) {
+      const lambda = node;
+      // Check for versioning property, exclude the case where the property
+      // can be a token (IResolvable).
+      lambda.addEnvironment("DD_EXTENSION_VERSION", "next");
+
+      // If no extension is set, this envs are not set
+      if (!this.datadog.props.extensionLayerVersion) {
+        lambda.addEnvironment("DD_VERSION", lambda.currentVersion.toString());
+
+        if (this.datadog.props.service) {
+          lambda.addEnvironment("DD_SERVICE", this.datadog.props.service);
+        }
+
+        if (this.datadog.props.env) {
+          lambda.addEnvironment("DD_ENV", this.datadog.props.env);
+        }
+
+        if (this.datadog.props.tags) {
+          lambda.addEnvironment("DD_TAGS", this.datadog.props.tags);
+        }
+      }
+
+      this.datadog.addLambdaFunctions([node]);
+    }
+  }
+}
---
+++ packages/datadog/test/aspect.test.ts
@@ -0,0 +1,131 @@
+import test from "node:test";
+
+import { Match, Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { DatadogLambda } from "datadog-cdk-constructs-v2";
+
+import { AddDatadogToLambdas } from "../src/lambdaAspect.js";
+
+test("should set envs and layers to all lambdas in stack", async (t) => {
+  await t.test("when extensionLayerVersion is true", () => {
+    const app = new cdk.App();
+
+    const testStack = new cdk.Stack(app, "TestStack");
+
+    new lambda.Function(testStack, "NativeLambda", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromInline("handler(){}"),
+      handler: "handler.handler",
+    });
+
+    const datadog = new DatadogLambda(testStack, `datadog`, {
+      site: "datadoghq.eu",
+      apiKeySecretArn: "arn:my-arn",
+      env: "test",
+      nodeLayerVersion: 115,
+      extensionLayerVersion: 65,
+      captureLambdaPayload: true,
+      logLevel: "warn",
+      service: "my-service",
+      tags: "my-tag1,my-tag2",
+      redirectHandler: true,
+    });
+
+    cdk.Aspects.of(app).add(new AddDatadogToLambdas("Datadog", { datadog }));
+
+    // Prepare the stack for assertions.
+    const template = Template.fromStack(testStack);
+
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Runtime: "nodejs20.x",
+      Layers: [
+        {
+          "Fn::Join": [
+            "",
+            [
+              "arn:aws:lambda:",
+              { Ref: "AWS::Region" },
+              Match.stringLikeRegexp("layer:Datadog-Node"),
+            ],
+          ],
+        },
+        {
+          "Fn::Join": [
+            "",
+            [
+              "arn:aws:lambda:",
+              { Ref: "AWS::Region" },
+              Match.stringLikeRegexp("layer:Datadog-Extension"),
+            ],
+          ],
+        },
+      ],
+      Environment: {
+        Variables: {
+          DD_EXTENSION_VERSION: "next",
+          DD_CAPTURE_LAMBDA_PAYLOAD: "true",
+          DD_LOG_LEVEL: "warn",
+          DD_ENV: "test",
+          DD_SERVICE: "my-service",
+          DD_TAGS: Match.stringLikeRegexp("my-tag1,my-tag2"),
+        },
+      },
+    });
+  });
+
+  await t.test("should set envs and layers to all lambdas in stack", () => {
+    const app = new cdk.App();
+
+    const testStack = new cdk.Stack(app, "TestStack");
+
+    new lambda.Function(testStack, "NativeLambda", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromInline("handler(){}"),
+      handler: "handler.handler",
+    });
+
+    const datadog = new DatadogLambda(testStack, `datadog`, {
+      site: "datadoghq.eu",
+      apiKeySecretArn: "arn:my-arn",
+      env: "test",
+      nodeLayerVersion: 115,
+      captureLambdaPayload: true,
+      logLevel: "warn",
+      service: "my-service",
+      tags: "my-tag1,my-tag2",
+      redirectHandler: true,
+    });
+
+    cdk.Aspects.of(app).add(new AddDatadogToLambdas("Datadog", { datadog }));
+
+    // Prepare the stack for assertions.
+    const template = Template.fromStack(testStack);
+
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Runtime: "nodejs20.x",
+      Layers: [
+        {
+          "Fn::Join": [
+            "",
+            [
+              "arn:aws:lambda:",
+              { Ref: "AWS::Region" },
+              Match.stringLikeRegexp("layer:Datadog-Node"),
+            ],
+          ],
+        },
+      ],
+      Environment: {
+        Variables: {
+          DD_EXTENSION_VERSION: "next",
+          DD_CAPTURE_LAMBDA_PAYLOAD: "true",
+          DD_LOG_LEVEL: "warn",
+          DD_ENV: "test",
+          DD_SERVICE: "my-service",
+          DD_TAGS: Match.stringLikeRegexp("my-tag1,my-tag2"),
+        },
+      },
+    });
+  });
+});
