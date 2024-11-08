f5593d5b5ba67a8ec357e543babeae7d3bcba4de
Brando Meniconi <b.meniconi@treedom.net>
2024-11-08 11:26:30 +0100

    fix: test rename and coverage

--- packages/datadog/test/lambda-aspect.test.ts
+++ packages/datadog/test/lambda-aspect.test.ts
@@ -143,3 +143,48 @@ test("should set envs and layers to all lambdas in stack", async (t) => {
     t.assert.match(tags.asString(), /lambda-tag2,?/);
   });
 });
+
+await test("should set DD_TAGS when specified in the constuct", (t) => {
+  const app = new cdk.App();
+
+  const testStack = new cdk.Stack(app, "TestStack");
+
+  new lambda.Function(testStack, "NativeLambda", {
+    runtime: lambda.Runtime.NODEJS_20_X,
+    code: lambda.Code.fromInline("handler(){}"),
+    handler: "test.handler",
+    environment: {
+      DD_TAGS: "lambda-tag1,lambda-tag2",
+    },
+  });
+
+  const datadog = new DatadogLambda(testStack, `datadog`, {
+    site: "datadoghq.eu",
+    apiKeySecretArn: "arn:my-arn",
+    env: "test",
+    nodeLayerVersion: 115,
+    captureLambdaPayload: true,
+    logLevel: "warn",
+    service: "my-service",
+    redirectHandler: false,
+    tags: "dd-tag1,dd-tag2",
+  });
+
+  cdk.Aspects.of(app).add(new AddDatadogToLambdas({ datadog }));
+
+  // Prepare the stack for assertions.
+  const template = Template.fromStack(testStack);
+
+  const tags = new Capture();
+
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: {
+      Variables: {
+        DD_TAGS: tags,
+      },
+    },
+  });
+
+  t.assert.match(tags.asString(), /dd-tag1,?/);
+  t.assert.match(tags.asString(), /dd-tag2,?/);
+});
